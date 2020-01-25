819169eabeadf0764b06c84d3299bec458c657d1
martanit <martanit@thinkuntu>
2020-01-25 18:35:12 +0100

    Fixed ctcf converter

--- utils/ctcf_convert.sh
+++ utils/ctcf_convert.sh
@@ -2,44 +2,19 @@
 
 input=$1
 
-awk 'NR==2{init_mon=$2/3000} {if ($1 == "chrX") printf("%0.f\t%.d\n", ($2/3000 - init_mon), $4)}' $input > ctcf_file.dat
-awk '(NR>1) && ($1!=p || $2!=s) {print p, s; s=0} {p=$1; s+=$2} END{print p, s}' ctcf_file.dat > ctcf_mod.dat
+init_mon=$((100378307/3000))
+max_mon=$((100702306/3000))
 
-p=$(awk '{print $1}' ctcf_mod.dat )
-v=$(awk '{print $2}' ctcf_mod.dat )
+awk -v x=$init_mon '{if ($1 == "chrX") printf("%0.f\t%.d\n", $2/3000-x, $4)}' $input > ctcf_file.dat
+
+p=$(awk '{print $1}' ctcf_file.dat)
+v=$(awk '{print $2}' ctcf_file.dat)
 
 p=($p)
 v=($v)
 
 ctcf_num=${#p[@]}
-ctcf_max=${p[$(($ctcf_num-1))]}
-
-for i in $(seq 0 $(($ctcf_num-1)))
-do
-	if [ "$i" != "0" ]
-	then
-		if [ "${p[$i]}" = "${p[$(($i+1))]}" ]
-		then
-			if [ "$((${p[$i]}-1))" != "${p[$(($i-1))]}" ]
-			then	
-				echo "$((${p[$i]}-1))   ${v[$i]}" >> ctcf_fin.dat
-			else 
-				if [ "$((${v[$((i-1))]}*${v[$i]}))" > "0" ]
-				then
-					echo "$((${p[$i]}-1))   $((${v[$(($i-1))]}+${v[$i]}))" >> ctcf_fin.dat
-				else
-					echo "$((${p[$i]}-1))   $((${v[$(($i-1))]}+${v[$(($i+1))]}))" >> ctcf_fin.dat
-				fi
-			fi	
-		else
-			echo "${p[$i]}   ${v[$i]}" >> ctcf_fin.dat
-		fi
-	else
-		echo "${p[$i]}   ${v[$i]}" >> ctcf_fin.dat
-	fi
-done
-
-for i in $(seq 0 $(($ctcf_max))) 
+for i in $(seq 0 $(($max_mon-$init_mon))) 
 do
     is_ctcf="0"
     for j in $(seq 0 $(($ctcf_num-1)))
