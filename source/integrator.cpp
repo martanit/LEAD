d80f473f22a2cc3535bca0c2a1c7ba8b43db5957
martanit <martina.crippa2@studenti.unimi.it>
2020-03-11 11:25:26 +0100

    Fixed permeability. To test

--- source/integrator.cpp
+++ source/integrator.cpp
@@ -24,47 +24,47 @@ void Integrator::markov_chain() {
         // right side go foward to right
         // tmp 10000
         if ((*i).get_r() != (m_poly.get_poly_nmonomers() - 1))
-            if (((*i).get_rate_fwr() * m_integrator_timestep * 10000 >
-                    transition_prob(mt)) and
-                    ((*i).get_ctcf()[(*i).get_r() + 1] >= 0 or
-                    (*i).get_rate_fwr() * (*i).get_permeability() /
+            if((((*i).get_ctcf()[(*i).get_r() + 1] >= 0 and 
+                 (*i).get_rate_fwr() * m_integrator_timestep * 10000 > 
+                    transition_prob(mt)) or 
+                ((*i).get_ctcf()[(*i).get_r() + 1] < 0 and
+                 (*i).get_rate_fwr() * (*i).get_permeability() /
                     std::abs((*i).get_ctcf()[(*i).get_r() + 1]) * 
-                    m_integrator_timestep *10000 > 
-                    transition_prob(mt)) and
+                    m_integrator_timestep * 10000 > transition_prob(mt))) and
                     !(m_vector_extr.overlap_rl(*i)) and
 		    !(m_vector_extr.overlap_rr(*i)))
                 (*i).set_r((*i).get_r() + 1);
         // right side go backward to left
-        if (((*i).get_rate_bwr() * m_integrator_timestep * 10000 > 
-                    transition_prob(mt)) and
-                ((*i).get_ctcf()[(*i).get_r() - 1] <= 0 or
+        if ((((*i).get_ctcf()[(*i).get_r() - 1] <= 0 and
+              (*i).get_rate_bwr() * m_integrator_timestep * 10000 > 
+                 transition_prob(mt)) or
+             ((*i).get_ctcf()[(*i).get_r() - 1] > 0 and
                  (*i).get_rate_bwr() * (*i).get_permeability() /
                  std::abs((*i).get_ctcf()[(*i).get_r() - 1]) *
-                 m_integrator_timestep * 10000 > 
-                 transition_prob(mt)) and
+                 m_integrator_timestep * 10000 > transition_prob(mt))) and
                 !(m_vector_extr.overlap_rr(*i)))
             (*i).set_r((*i).get_r() - 1);
 
         // left side go forward to left
         if ((*i).get_l() != 0)
-            if (((*i).get_rate_fwl() * m_integrator_timestep * 10000 >
-                    transition_prob(mt)) and
-                    ((*i).get_ctcf()[(*i).get_l() - 1] <= 0 or
-                     (*i).get_rate_fwl() * (*i).get_permeability() /
+            if ((((*i).get_ctcf()[(*i).get_l() - 1] <= 0 and
+                  (*i).get_rate_fwl() * m_integrator_timestep * 10000 >
+                    transition_prob(mt)) or
+                 ((*i).get_ctcf()[(*i).get_l() - 1] > 0 or
+                  (*i).get_rate_fwl() * (*i).get_permeability() /
                      std::abs((*i).get_ctcf()[(*i).get_l() - 1]) * 
-                     m_integrator_timestep * 10000 > 
-                     transition_prob(mt)) and
+                     m_integrator_timestep * 10000 > transition_prob(mt))) and
                     !(m_vector_extr.overlap_lr(*i)) and
 		    !(m_vector_extr.overlap_ll(*i)))
                 (*i).set_l((*i).get_l() - 1);
         // left side go backward to right
-        if (((*i).get_rate_bwl() * m_integrator_timestep * 10000 > 
-                    transition_prob(mt)) and
-                ((*i).get_ctcf()[(*i).get_l() + 1] >= 0 or
-                 (*i).get_rate_bwl() * (*i).get_permeability() /
+        if ((((*i).get_ctcf()[(*i).get_l() + 1] >= 0 and
+              (*i).get_rate_bwl() * m_integrator_timestep * 10000 > 
+                 transition_prob(mt)) or
+             ((*i).get_ctcf()[(*i).get_l() + 1] < 0 or
+              (*i).get_rate_bwl() * (*i).get_permeability() /
                  std::abs((*i).get_ctcf()[(*i).get_l() + 1]) *
-                 m_integrator_timestep *10000 > 
-                 transition_prob(mt)) and
+                 m_integrator_timestep *10000 >  transition_prob(mt))) and
                 !(m_vector_extr.overlap_ll(*i)))
             (*i).set_l((*i).get_l() + 1);
     }
