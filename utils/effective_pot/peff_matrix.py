4883e004a924004148b41c9877e7c223620e104c
martanit <martina.crippa2@studenti.unimi.it>
2020-07-30 15:49:30 +0200

    Update effective potential script

--- utils/effective_pot/peff_matrix.py
+++ utils/effective_pot/peff_matrix.py
@@ -69,7 +69,8 @@ for i in range(0,N-2):
         #(g) i<->--<--j
         #(h) i<->--<->j
         if(cr>0 and cl>0):
-            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]!=(kcr[0]-1)):
+           # if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]!=(kcr[0]-1)):
+            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]>kcr[0]):
                 P_eff[i,j]=0
 
         #one or several same oriented ctcf between i and j
@@ -88,14 +89,14 @@ for i in range(0,N-2):
         if(cl>0 and cr==0):
             #(j) i--<--<--j
             if(i!=kcl[0] and j!=kcl[cl-1]):
-                n=kcl[0]-i
-                m=j-kcl[cl-1]
+                m=kcl[0]-i
+                n=j-kcl[cl-1]
                 P_eff[i,j]=choose(n+m-1,m)*hyp2f1(1,1-n,1+m,-1)*(kfw/(2*(koff+kfw)))**(j-i-1)*p0
         if(cl>1 and cr==0):        
             #(l) i<--<--<--j
             if(i==kcl[0] and j!=kcl[cl-1]):
-                n=kcl[1]-i
-                m=j-kcl[cl-1]
+                m=kcl[1]-i
+                n=j-kcl[cl-1]
                 P_eff[i,j]=choose(n+m-1,m)*hyp2f1(1,1-n,1+m,-1)*(kfw/(2*(koff+kfw)))**(j-i-1)*p0
         if(cr>0 and cl==1):
             #(m) i<->-->--j
@@ -112,42 +113,41 @@ for i in range(0,N-2):
         if(cl>0 and cr==1):
             #(n) i--<--<->j
             if(j==kcr[cr-1] and i!=kcl[0]):
-                n=kcl[0]-i
-                m=j-kcl[cl-1]
+                m=kcl[0]-i
+                n=j-kcl[cl-1]
                 P_eff[i,j]=choose(n+m-1,m)*hyp2f1(1,1-n,1+m,-1)*(kfw/(2*(koff+kfw)))**(j-i-1)*p0
         if(cl>1 and cr==1):        
+            #(o) i<--<--<-->j
             if(i==kcl[0] and j==kcr[cr-1]):
-                #(o) i<--<--<-->j
-                n=kcl[1]-i
-                m=j-kcl[cl-1]
+                m=kcl[1]-i
+                n=j-kcl[cl-1]
                 P_eff[i,j]=choose(n+m-1,m)*hyp2f1(1,1-n,1+m,-1)*(kfw/(2*(koff+kfw)))**(j-i-1)*p0
         
         #two or more ctcf with divergent orientation
         if(cr>0 and cl>0):
             #(q) i---<<-->>---j
-            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<=(kcr[0]-1) and i!=kcl[0] and j!=kcr[cr-1]):
+            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<kcr[0] and i!=kcl[0] and j!=kcr[cr-1]):
                 n=kcl[0]-i
                 m=j-kcr[cr-1]
                 P_eff[i,j]=((kfw/2.)/(koff+kfw))**(j-i-1)*(choose(m-1,m)*hyp2f1(1,1,1+m,-1)*((kfw/2.)/(koff+kfw/2.))**n+\
                                                            choose(n-1,0)*hyp2f1(1,1-n,1,-1)*((kfw/2.)/(koff+kfw/2.))**m)*p0
-                #print(i,j,P_eff[i,j])
         if(cr>0 and cl>1):
             #(r) i<--<<-->>---j   
-            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<=(kcr[0]-1) and i==kcl[0] and j!=kcr[cr-1]):
+            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<kcr[0] and i==kcl[0] and j!=kcr[cr-1]):
                 n=kcl[1]-i
                 m=j-kcr[cr-1]
                 P_eff[i,j]=((kfw/2.)/(koff+kfw))**(j-i-1)*(choose(m-1,m)*hyp2f1(1,1,1+m,-1)*((kfw/2.)/(koff+kfw/2.))**n+\
                                                            choose(n-1,0)*hyp2f1(1,1-n,1,-1)*((kfw/2.)/(koff+kfw/2.))**m)*p0
         if(cr>1 and cl>0):
             #(s) i---<<-->>-->j
-            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<=(kcr[0]-1) and i!=kcl[0] and j==kcr[cr-1]):
+            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<kcr[0] and i!=kcl[0] and j==kcr[cr-1]):
                 n=kcl[0]-i
                 m=j-kcr[cr-2]
                 P_eff[i,j]=((kfw/2.)/(koff+kfw))**(j-i-1)*(choose(m-1,m)*hyp2f1(1,1,1+m,-1)*((kfw/2.)/(koff+kfw/2.))**n+\
                                                            choose(n-1,0)*hyp2f1(1,1-n,1,-1)*((kfw/2.)/(koff+kfw/2.))**m)*p0
         if(cr>1 and cl>1):
             #(t) i<--<<-->>-->j
-            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<=(kcr[0]-1) and i==kcl[0] and j==kcr[cr-1]):
+            if(i!=kcr[0] and j!=kcl[cl-1] and kcl[cl-1]<kcr[0] and i==kcl[0] and j==kcr[cr-1]):
                 n=kcl[1]-i
                 m=j-kcr[cr-2]
                 P_eff[i,j]=((kfw/2.)/(koff+kfw))**(j-i-1)*(choose(m-1,m)*hyp2f1(1,1,1+m,-1)*((kfw/2.)/(koff+kfw/2.))**n+\
@@ -171,7 +171,7 @@ for i in range(0,N-2):
         #(w) i>-->--<--j
         #(x) i>-->--<->j
         if(cr>1 and cl>0):
-            if(i==kcr[0] and j!=kcl[cl-1] and kcl[cl-1]!=(kcr[1]-1)):
+            if(i==kcr[0] and j!=kcl[cl-1] and kcl[cl-1]>kcr[1]):
                 P_eff[i,j]=0
 
         #one or several same oriented ctcf between i and j
@@ -193,29 +193,29 @@ for i in range(0,N-2):
                 m2=j-kcr[cr-2]-1
                 P_eff[i,j]=(kfw/2.)/(koff+kfw/2.)*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+\
                                                    choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
-        #(z) i>--<--<--j
         if(cl>0 and cr==1):
+            #(z) i>--<--<--j
             if(i==kcr[0] and j!=kcl[cl-1] and j!=kcr[cr-1]):
-                n1=kcl[0]-i-1
-                m1=j-kcl[cl-1]
-                n2=kcl[0]-i
-                m2=j-kcl[cl-1]-1
+                m1=kcl[0]-i-1
+                n1=j-kcl[cl-1]
+                m2=kcl[0]-i
+                n2=j-kcl[cl-1]-1
                 P_eff[i,j]=(kfw/2.)/(koff+kfw/2.)*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+\
                                                    choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
-        #(b') i>--<--<->j         
         if(cl>0 and cr==2):
+            #(b') i>--<--<->j         
             if(i==kcr[0] and j==kcr[cr-1]):
-                n1=kcl[0]-i-1
-                m1=j-kcl[cl-1]
-                n2=kcl[0]-i
-                m2=j-kcl[cl-1]-1
+                m1=kcl[0]-i-1
+                n1=j-kcl[cl-1]
+                m2=kcl[0]-i
+                n2=j-kcl[cl-1]-1
                 P_eff[i,j]=(kfw/2.)/(koff+kfw/2.)*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+\
                                                    choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
         
         #two or more ctcf with divergent orientation
         if(cr>1 and cl>0):
             #(c') i>--<<-->>--j
-            if(i==kcr[0] and j!=kcl[cl-1] and j!=kcr[cr-1] and kcl[cl-1]<=(kcr[1]-1)):
+            if(i==kcr[0] and j!=kcl[cl-1] and j!=kcr[cr-1] and kcl[cl-1]<kcr[1]):
                 n1=kcl[0]-i-1
                 m1=j-kcr[cr-1]
                 n2=kcl[0]-i
@@ -226,7 +226,7 @@ for i in range(0,N-2):
                                           choose(n2-1,0)*hyp2f1(1,1-n2,1,-1)*((kfw/2.)/(koff+kfw))**(n2+kcr[cr-1]-kcl[0]-1)*((kfw/2.)/(koff+kfw/2.))**m2)*p0
         if(cr>2 and cl>0):
             #(d') i>--<<-->>->j
-            if(i==kcr[0] and j==kcr[cr-1] and kcl[cl-1]<=(kcr[1]-1)):
+            if(i==kcr[0] and j==kcr[cr-1] and kcl[cl-1]<kcr[1]):
                 n1=kcl[0]-i-1
                 m1=j-kcr[cr-2]
                 n2=kcl[0]-i
@@ -261,19 +261,19 @@ for i in range(0,N-2):
         if(cl>1 and cr==0):
             #(i') i--<--<-<j
             if(j==kcl[cl-1] and i!=kcl[0]):
-                n1=kcl[0]-i-1
-                m1=j-kcl[cl-2]
-                n2=kcl[0]-i
-                m2=j-kcl[cl-2]-1
+                m1=kcl[0]-i-1
+                n1=j-kcl[cl-2]
+                m2=kcl[0]-i
+                n2=j-kcl[cl-2]-1
                 P_eff[i,j]=(kfw/2.)/(koff+kfw/2.)*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+\
                                                    choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
         if(cl>2 and cr==0):
             #(k') i<-<--<-<j
             if(j==kcl[cl-1] and i==kcl[0]):
-                n1=kcl[1]-i-1
-                m1=j-kcl[cl-2]
-                n2=kcl[1]-i
-                m2=j-kcl[cl-2]-1
+                m1=kcl[1]-i-1
+                n1=j-kcl[cl-2]
+                m2=kcl[1]-i
+                n2=j-kcl[cl-2]-1
                 P_eff[i,j]=(kfw/2.)/(koff+kfw/2.)*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+\
                                                    choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
         #(j') i-->-->-<j
@@ -298,7 +298,7 @@ for i in range(0,N-2):
         #two or more ctcf with divergent orientation
         if(cr>0 and cl>1):
             #(m') i--<<-->>-<j
-            if(i!=kcr[0] and j==kcl[cl-1] and i!=kcl[0] and kcl[cl-2]<=(kcr[0]-1)):
+            if(i!=kcr[0] and j==kcl[cl-1] and i!=kcl[0] and kcl[cl-2]<kcr[0]):
                 n1=kcl[0]-i-1
                 m1=j-kcr[cr-1]
                 n2=kcl[0]-i
@@ -309,7 +309,7 @@ for i in range(0,N-2):
                                           choose(n2-1,0)*hyp2f1(1,1-n2,1,-1)*((kfw/2.)/(koff+kfw))**(n2+kcr[cr-1]-kcl[0]-1)*((kfw/2.)/(koff+kfw/2.))**m2)*p0
         if(cr>0 and cl>2):
             #(n') i<--<<-->>-<j
-            if(i==kcl[0] and j==kcl[cl-1] and kcl[cl-2]<=(kcr[0]-1)):
+            if(i==kcl[0] and j==kcl[cl-1] and kcl[cl-2]<kcr[0]):
                 n1=kcl[1]-i-1
                 m1=j-kcr[cr-1]
                 n2=kcl[1]-i
@@ -331,7 +331,7 @@ for i in range(0,N-2):
         #two or more ctcf with convergent orientation
         #(p') i>->--<-<j
         if(cr>1 and cl>1):
-            if(i==kcr[0] and j==kcl[cl-1] and kcl[cl-2]!=(kcr[1]-1)):
+            if(i==kcr[0] and j==kcl[cl-1] and kcl[cl-2]>kcr[1]):
                 P_eff[i,j]=0
         
         #one or several same oriented ctcf between i and j
@@ -347,16 +347,16 @@ for i in range(0,N-2):
         #(r') i>-<--<-<j
         if(cl>1 and cr==1):
             if(i==kcr[0] and j==kcl[cl-1]):
-                n1=kcl[0]-i-1
-                m1=j-kcl[cl-2]
-                n2=kcl[0]-i
-                m2=j-kcl[cl-2]-1
+                m1=kcl[0]-i-1
+                n1=j-kcl[cl-2]
+                m2=kcl[0]-i
+                n2=j-kcl[cl-2]-1
                 P_eff[i,j]=(kfw/2.)/koff*(choose(n1+m1-1,m1)*hyp2f1(1,1-n1,1+m1,-1)+choose(n2+m2-1,m2)*hyp2f1(1,1-n2,1+m2,-1))*(kfw/(2*(koff+kfw)))**(j-i-2)*p0
         
         #two or more ctcf with divergent orientation
         if(cr>1 and cl>1):
             #(s') i>--<<-->>--<j
-            if(i==kcr[0] and j==kcl[cl-1] and kcl[cl-2]<=(kcr[1]-1)):
+            if(i==kcr[0] and j==kcl[cl-1] and kcl[cl-2]<kcr[1]):
                 n1=kcl[0]-i-1
                 m1=j-kcr[cr-1]
                 n2=kcl[0]-i
